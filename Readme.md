723fb149dbaef3481ef623ad12f71ffb03556341
sinoe9891 <sinoeproducciones@gmail.com>
2021-04-08 00:37:10 -0600

    Finalización de Cotización en línea con JS

--- Readme.md
+++ Readme.md
@@ -10,19 +10,19 @@
 1. **Creando los Testimoniales (3/3 | 17min)** :white_check_mark:
 1. **Sección de Newsletter y Cuenta regresiva (4/4 | 12min)** :white_check_mark:
 1. **Sección de Registro con HTML y CSS (7/7 | 39min)** :white_check_mark:
-1. **Cotización en línea con JavaScript (11/11 | 56min)** :ballot_box_with_check:
+1. **Cotización en línea con JavaScript (11/11 | 56min)** :white_check_mark:
 	-  Esperando que el HTML este listo :white_check_mark:
 	-  Creando las variables de nuestros campos :white_check_mark:
 	-  Habilitando el botón calcular basado en seleciones del usuario :white_check_mark:
 	-  Calculos con JavaScript para cobrar según el boleto adquirido :white_check_mark:
 	-  Asegurándonos que los datos que recibimos son números :white_check_mark:
-	-  Elaborando la sección de Resumen
-	-  CSS el resumen de compra
-	-  Ocultar y Mostrar las conferencias basadas en el día
-	-  Validando con JavaScript
-	-  Creando una función para Validar
-	-  Validando nuestro formulario para recibir emails
-1. **Agregando un Mapa con LeafletJS (0/3 | 9min)** :x:
+	-  Elaborando la sección de Resumen :white_check_mark:
+	-  CSS el resumen de compra :white_check_mark:
+	-  Ocultar y Mostrar las conferencias basadas en el día :white_check_mark:
+	-  Validando con JavaScript :ballot_box_with_check:
+	-  Creando una función para Validar :ballot_box_with_check:
+	-  Validando nuestro formulario para recibir emails :ballot_box_with_check:
+1. **Agregando un Mapa con LeafletJS (0/3 | 9min)** :ballot_box_with_check:
 1. **Introducción a jQuery** :x:
 1. **Primeros Pasos con jQuery** :x:
 1. **jQuery en nuestro sitio web** :x:
--- js/main.js
+++ js/main.js
@@ -1,98 +1,163 @@
 // Funcion de espera que cargue el contenido
-(function() {
-    "use strict";
-    //Select
-    var regalo = document.getElementById('regalo');
+(function () {
+	"use strict";
+	//Select
+	var regalo = document.getElementById('regalo');
 
-    document.addEventListener('DOMContentLoaded', function() {
-            console.log("listo");
+	document.addEventListener('DOMContentLoaded', function () {
+		// console.log("listo");
 
-            //CAMPOS DATOS USUARIO
-            var nombre = document.getElementById('nombre');
-            var apellido = document.getElementById('apellido');
-            var email = document.getElementById('email');
+		//CAMPOS DATOS USUARIO
+		var nombre = document.getElementById('nombre');
+		var apellido = document.getElementById('apellido');
+		var email = document.getElementById('email');
 
-            //CAMPOS PASES
-            var pase_dia = document.getElementById('pase_dia');
-            var pase_dosdias = document.getElementById('pase_dosdias');
-            var pase_completo = document.getElementById('pase_completo');
+		//CAMPOS PASES
+		var pase_dia = document.getElementById('pase_dia');
+		var pase_dosdias = document.getElementById('pase_dosdias');
+		var pase_completo = document.getElementById('pase_completo');
 
-            //BOTONES
-            var calcular = document.getElementById('calcular');
-            var errorDiv = document.getElementById('error');
-            var botonRegistro = document.getElementById('btnRegistro');
-            var resultado = document.getElementById('lista-productos');
-            var lista_productos = document.getElementById('lista-productos');
-            var lista_box = document.getElementById('lista');
-            var suma = document.getElementById('sumatotal');
-			
-            //EXTRAS
-            var camisas = document.getElementById('camisa_evento');
-            var etiquetas = document.getElementById('etiquetas');
-            var error_regalo = document.getElementById('error_regalo')
+		//BOTONES
+		var calcular = document.getElementById('calcular');
+		var errorDiv = document.getElementById('error');
+		var botonRegistro = document.getElementById('btnRegistro');
+		var lista_productos = document.getElementById('lista-productos');
+		var lista_box = document.getElementById('lista');
+		var suma = document.getElementById('sumatotal');
 
-
-            calcular.addEventListener('click', calcularMontos);
+		//EXTRAS
+		var camisas = document.getElementById('camisa_evento');
+		var etiquetas = document.getElementById('etiquetas');
+		var error_regalo = document.getElementById('error_regalo')
 
 
-            function calcularMontos(event) {
-                event.preventDefault();
-                //console.log("Has hecho click en calcular");
-                if (regalo.value === '') {
-                    // alert("Debes elegir un regalo");
-                    error_regalo.innerHTML = "<strong><span>Debes elegir un regalo</span></strong>";
-                    regalo.focus();
-                } else {
-                    var boletosDia = parseInt(pase_dia.value, 10) || 0,
-                        boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
-                        boletosCompleto = parseInt(pase_completo.value, 10) || 0,
-                        cantCamisas = parseInt(camisas.value, 10) || 0,
-                        cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
-                    // console.log(pase_dia.value);
-                    // console.log(pase_dosdias.value);
-                    // console.log(pase_completo.value);
+		calcular.addEventListener('click', calcularMontos);
 
-                    // console.log("Boltos día: " + boletosDia);
-                    // console.log("Boltos 2 días: " + boletos2Dias);
-                    // console.log("Boltos Completos: " + boletosCompleto);
+		pase_dia.addEventListener('blur', mostrarDias);
+		pase_dosdias.addEventListener('blur', mostrarDias);
+		pase_completo.addEventListener('blur', mostrarDias);
+
+		//VALIDAR INPUTS
+		nombre.addEventListener('blur', validarCampos);
+		apellido.addEventListener('blur', validarCampos);
+		email.addEventListener('blur', validarCampos);
+		email.addEventListener('blur', validarEmail);
 
 
-                    var totalPagar = (boletosDia * 30) + (boletos2Dias * 45) + (boletosCompleto * 50) +
-                        ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
-                    // console.log(totalPagar);
+		function validarCampos() {
+			if (this.value == '') {
+				errorDiv.style.display = 'block';
+				errorDiv.innerHTML = "Este campo es obligatorio";
+				this.style.border = '1px solid red';
+			} else {
+				errorDiv.style.display = 'none';
+				this.style.border = '1px solid #ccc';
+			}
+		}
 
-                    var listadoProductos = [];
-                    if (boletosDia >= 1) {
-                        listadoProductos.push(boletosDia + ' Pases por día');
-                    }
-                    if (boletos2Dias >= 1) {
-                        listadoProductos.push(boletos2Dias + ' Pases por 2 días');
-                    }
-                    if (boletosCompleto >= 1) {
-                        listadoProductos.push(boletosCompleto + ' Pases Completos ');
-                    }
-                    if (cantCamisas >= 1) {
-                        listadoProductos.push(cantCamisas + ' Camisas ');
-                    }
-                    if (cantEtiquetas >= 1) {
-                        listadoProductos.push(cantEtiquetas + ' Etiquetas');
-                    }
-
-                    lista_box.style.display = "block";
-                    lista_productos.innerHTML = '';
-                    for (let i = 0; i < listadoProductos.length; i++) {
-                        lista_productos.innerHTML += "<li><span>" + listadoProductos[i] + "</span></li>"
-                    }
+		function validarEmail() {
+			if (this.value.indexOf("@") > -1) {
+				errorDiv.style.display = 'none';
+				this.style.border = '1px solid #ccc';
+			} else {
+				errorDiv.style.display = 'block';
+				errorDiv.innerHTML = "Formato no válido";
+				this.style.border = '1px solid red';
+			}
+		}
 
 
-                    suma.innerHTML = "<strong>$ " + totalPagar.toFixed(2) + "</strong>";
+		function calcularMontos(event) {
+			event.preventDefault();
+			//console.log("Has hecho click en calcular");
+			if (regalo.value === '') {
+				// alert("Debes elegir un regalo");
+				error_regalo.innerHTML = "<strong><span>Debes elegir un regalo</span></strong>";
+				regalo.focus();
+			} else {
+				var boletosDia = parseInt(pase_dia.value, 10) || 0,
+					boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
+					boletosCompleto = parseInt(pase_completo.value, 10) || 0,
+					cantCamisas = parseInt(camisas.value, 10) || 0,
+					cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+				// console.log(pase_dia.value);
+				// console.log(pase_dosdias.value);
+				// console.log(pase_completo.value);
+
+				// console.log("Boltos día: " + boletosDia);
+				// console.log("Boltos 2 días: " + boletos2Dias);
+				// console.log("Boltos Completos: " + boletosCompleto);
 
 
-                    console.log(listadoProductos);
+				var totalPagar = (boletosDia * 30) + (boletos2Dias * 45) + (boletosCompleto * 50) +
+					((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+				// console.log(totalPagar);
 
-                }
-            }
+				var listadoProductos = [];
+				if (boletosDia >= 1) {
+					listadoProductos.push(boletosDia + ' Pases por día');
+				}
+				if (boletos2Dias >= 1) {
+					listadoProductos.push(boletos2Dias + ' Pases por 2 días');
+				}
+				if (boletosCompleto >= 1) {
+					listadoProductos.push(boletosCompleto + ' Pases Completos ');
+				}
+				if (cantCamisas >= 1) {
+					listadoProductos.push(cantCamisas + ' Camisas ');
+				}
+				if (cantEtiquetas >= 1) {
+					listadoProductos.push(cantEtiquetas + ' Etiquetas');
+				}
+
+				lista_box.style.display = "block";
+				lista_productos.innerHTML = '';
+				for (let i = 0; i < listadoProductos.length; i++) {
+					lista_productos.innerHTML += "<li><span>" + listadoProductos[i] + "</span></li>"
+				}
 
 
-        }) //DOM CONTENT LOADED
+				suma.innerHTML = "<strong>$ " + totalPagar.toFixed(2) + "</strong>";
+
+
+				console.log(listadoProductos);
+
+			}
+		}
+
+		function mostrarDias() {
+			let boletosDia = parseInt(pase_dia.value, 10) || 0,
+				boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
+				boletosCompleto = parseInt(pase_completo.value, 10) || 0;
+
+			let diasElegidos = [];
+
+			//aca uso push el profe pero yo preferi igualar para que los pueda ocultar
+			if (boletosDia > 0) {
+				diasElegidos = ["viernes"];
+			}
+			if (boletos2Dias > 0) {
+				diasElegidos = ["viernes", "sabado"];
+			}
+			if (boletosCompleto > 0) {
+				diasElegidos = ["viernes", "sabado", "domingo"];
+			}
+			console.log(diasElegidos);
+			for (let i = 0; i < diasElegidos.length; i++) {
+				document.getElementById(diasElegidos[i]).style.display = "block";
+			}
+
+			//Para ocultar si vuelven a 0
+			if (diasElegidos.length < 3) {
+				document.getElementById("domingo").style.display = "none";
+			}
+			if (diasElegidos.length < 2) {
+				document.getElementById("sabado").style.display = "none";
+			}
+			if (diasElegidos.length < 1) {
+				document.getElementById("viernes").style.display = "none";
+			}
+		}
+
+	}) //DOM CONTENT LOADED
 })();
